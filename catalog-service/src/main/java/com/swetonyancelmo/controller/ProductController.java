7d2e96b7a6d04b3be9fa1cc60eb1096cf835296f
Swetony <swetonyancelmo@gmail.com>
2026-07-07 16:12:25 -0300

    fix: remove documentação swagger de product service

--- catalog-service/src/main/java/com/swetonyancelmo/controller/ProductController.java
+++ catalog-service/src/main/java/com/swetonyancelmo/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.swetonyancelmo.controller;
 import com.swetonyancelmo.dto.CreateProductDTO;
 import com.swetonyancelmo.dto.ProductResponseDTO;
 import com.swetonyancelmo.service.ProductService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,28 +10,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Tag(name = "Product Controller", description = "API para gerenciamento de produtos")
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-public class ProductController implements com.swetonyancelmo.controller.docs.ProductControllerDocs {
+public class ProductController {
 
     private final ProductService service;
 
     @GetMapping
-    @Override
     public ResponseEntity<List<ProductResponseDTO>> getProducts() {
         return ResponseEntity.ok(service.getAllProducts());
     }
 
     @GetMapping("/{id}")
-    @Override
     public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
         return ResponseEntity.ok(service.getProductById(id));
     }
 
     @PostMapping
-    @Override
     public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody CreateProductDTO dto) {
         return new ResponseEntity<>(service.createProduct(dto), HttpStatus.CREATED);
     }
